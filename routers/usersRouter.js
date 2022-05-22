5ee0ceb878fd6fb54a86428789782ee194e0413b
SuhravHussen <suhravshan@gmail.com>
2022-05-22 20:26:05 +0600

    role base acces control added & project done

--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const { getUsers, addUser, removeUser } = require('../controllers/usersController');
-const { checkLogin } = require('../middleware/common/checkLogin');
+const { checkLogin , requireRole } = require('../middleware/common/checkLogin');
 const  decorateHtmlResponse = require('../middleware/common/decorateHtmlResponse');
 const avatarUpload = require('../middleware/users/avaterUpload');
 const { addUserValidators, addUserValidationHandler } = require('../middleware/users/userValidators');
 const router = express.Router();
 
 // user page
-router.get('/',decorateHtmlResponse("Users") , checkLogin , getUsers);
+router.get('/',decorateHtmlResponse("Users") , checkLogin , requireRole(['admin']) , getUsers);
 
 // add user
-router.post('/',checkLogin , avatarUpload , addUserValidators, addUserValidationHandler , addUser);
+router.post('/',checkLogin ,  requireRole(['admin']) ,avatarUpload , addUserValidators, addUserValidationHandler , addUser);
 
 // remove user
-router.delete('/:id' , removeUser)
+router.delete('/:id'  ,requireRole(['admin']) , removeUser)
 
 module.exports = router;
